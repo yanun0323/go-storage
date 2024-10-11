b4efe01771becead5c104d0db89290ba5d1c80fa
yanun0323 <a1256212562@gmail.com>
2024-10-11 08:22:30 +0800

    fix: update file content mysql type

--- internal/entity.go
+++ internal/entity.go
@@ -3,7 +3,7 @@ package internal
 type File struct {
 	ID        string `gorm:"column:id;type:varchar(255);primaryKey"`
 	Type      string `gorm:"column:type;type:varchar(255);not null"`
-	Content   []byte `gorm:"column:blob;type:blob;not null"`
+	Content   []byte `gorm:"column:blob;type:longblob;not null"`
 	CreatedAt int64  `gorm:"column:created_at;type:bigint;not null;autoCreateTime"`
 }
 
